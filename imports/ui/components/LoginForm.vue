5ab39a3435319dcbba38bf02ab714bacb99057a1
Alok <alokaryal32@gmail.com>
2022-09-23 14:39:20 +0545

    First commit

---
+++ imports/ui/components/LoginForm.vue
@@ -0,0 +1,74 @@
+<template>
+  <form class="login-form" @submit.prevent="handleSubmit">
+    <div class="forms">
+      <label for="username">Username</label>
+      <input
+        id="username"
+        name="username"
+        type="text"
+        placeholder="Enter Username"
+        required
+        v-model="username"
+      />
+    </div>
+
+    <div>
+      <label for="password">Password</label>
+      <input
+        id="password"
+        name="password"
+        type="password"
+        placeholder="Enter Password"
+        required
+        v-model="password"
+      />
+    </div>
+    <div class="img">
+      <img v-bind:src="images" />
+    </div>
+
+    <div>
+      <button type="submit">Log In</button>
+    </div>
+  </form>
+</template>
+
+<script>
+import { Meteor } from "meteor/meteor";
+
+export default {
+  name: "LoginForm",
+  data() {
+    return {
+      username: "",
+      password: "",
+      images: require("../../images/todo-pic.jpg"),
+    };
+  },
+  methods: {
+    handleSubmit(event) {
+      Meteor.loginWithPassword(this.username, this.password);
+    },
+  },
+};
+</script>
+<style scoped>
+form {
+  margin: 70px 10px 10px 450px;
+  border: 4px solid gray;
+  height: 500px;
+  width: 800px;
+  border-radius: 28px;
+  box-shadow: 5px 8px 5px 7px;
+}
+.images {
+  /* position: absolute;k */
+}
+input {
+  border-radius: 12px;
+}
+button {
+  padding: 12px;
+  border-radius: 12px;
+}
+</style>
---
+++ imports/ui/components/cosmestic.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Hello</p>
+    <p>Hello everyone</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
